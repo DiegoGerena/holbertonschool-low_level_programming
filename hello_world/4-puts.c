1d5682b343b8cf8221b05cc2455e9acb2e0e7fb8
DiegoGerena <5010@holbertonschool.com>
2022-06-06 17:43:18 +0000

    c file, Programming is like building a multilingual puzzle

--- hello_world/4-puts.c
+++ hello_world/4-puts.c
@@ -1,6 +1,9 @@
 #include <stdio.h>
 /**
+*char - assign a variable Mystr
+*puts - this prints a string
 * description: print a string using puts function, avoid using printf function
+*Return: the string text
 */
 int main(void)
 {
