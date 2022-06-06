34412f98512cdc06e63131ff32bf226595f6ee69
DiegoGerena <5010@holbertonschool.com>
2022-06-06 18:00:26 +0000

    print function

---
+++ hello_world/5-printf.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+* main - a program to print a string using print function,
+* Return: value of 0
+**/
+int main(void)
+{
+	printf ("with proper grammar, but the outcome is a piece of art,\n");
+
+	return (0);
+}
