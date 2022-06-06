5ebab19ed2a1d0a3a1d3e565f6e351dda4b00b7a
DiegoGerena <5010@holbertonschool.com>
2022-06-06 16:01:17 +0000

    c file, Programming is like building a multilingual puzzle

--- hello_world/4-puts.c
+++ hello_world/4-puts.c
@@ -1,7 +1,8 @@
 #include <stdio.h>
-int main(void)/*print using puts, not printf.*/
+int main(void) /*print using puts, not printf.*/
 
 {
+/*print using puts, not printf.*/
 	char Mystr[] = "\"Programming is like building a multilingual puzzle";
 
 	puts(Mystr);
