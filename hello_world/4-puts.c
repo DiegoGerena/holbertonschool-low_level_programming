08a69edf9c4265ea717fd50483e74fa6ed307161
DiegoGerena <5010@holbertonschool.com>
2022-06-06 15:56:18 +0000

    c file, Programming is like building a multilingual puzzle

--- hello_world/4-puts.c
+++ hello_world/4-puts.c
@@ -1,8 +1,10 @@
 #include <stdio.h>
+/*Write a C program that prints exactly "Programming is like building a multilingual puzzle, followed by a new line.*/
 int main(void)
 
 {
 	char Mystr[] = "\"Programming is like building a multilingual puzzle";
+	
 	puts(Mystr);
 
 	return (0);
